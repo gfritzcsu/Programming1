2982907951b52d59c927fece91b70abf21497f9b
gfritz <gregory.fritz@csuglobal.edu>
2025-06-15 13:15:54 -0600

    removed psuedocode file and condensed to top of java file as well as added java file for assignment

---
+++ CriticalThinkingMod1Opt1Fritz.java
@@ -0,0 +1,35 @@
+//  Program Name: Print Info of Fake Person
+//  Author: Greg Fritz
+//  Date: 15 June 2025
+// -------------------------------------------
+//  Pseudocode:
+// -------------------------------------------
+// BEGIN
+//   SET firstName TO "John"
+//   SET lastName TO "Doe"
+//   SET streetAddress TO "123 John Doe Street"
+//   SET city TO "Somecity"
+//   SET zipCode TO "12345"
+
+//   PRINT firstName
+//   PRINT lastName
+//   PRINT streetAddress
+//   PRINT city
+//   PRINT zipCode
+// END
+
+public class CriticalThinkingMod1Opt1Fritz {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "1234 Main Street";
+        String city = "Springfield";
+        String zipCode = "12345";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
